6b9194f74e2545ff2264a55bc2cb58679a114e95
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-04-16 07:13:58 -0700

    Create VideoLesson Model

---
+++ SpeakingInBits/Models/Lesson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeakingInBits.Models
+{
+    /// <summary>
+    /// Generic course lesson
+    /// </summary>
+    public abstract class Lesson
+    {
+        [Key]
+        public int LessonId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+    }
+
+
+    /// <summary>
+    /// Video lecture lesson
+    /// </summary>
+    public class VideoLesson : Lesson
+    {
+        /// <summary>
+        /// Embed code for video. ex. YouTube/Vimeo
+        /// </summary>
+        public string EmbedCode { get; set; }
+    }
+}
