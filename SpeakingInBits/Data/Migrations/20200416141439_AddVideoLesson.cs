6ac25d37d8d8526fbd95ff2b4c0b97c203177c36
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-04-16 07:15:22 -0700

    Add Migration for video lessons

---
+++ SpeakingInBits/Data/Migrations/20200416141439_AddVideoLesson.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpeakingInBits.Data.Migrations
+{
+    public partial class AddVideoLesson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideoLessons",
+                columns: table => new
+                {
+                    LessonId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    EmbedCode = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoLessons", x => x.LessonId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VideoLessons");
+        }
+    }
+}
